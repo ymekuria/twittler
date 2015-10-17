fb0cd3b9a6692fe808502c54dbd7b13d32218565
Yoni <ymekuria@gmail.com>
2015-10-17 00:26:03 -0700

    'Added styles for the timestamps, and hover state styling for the usernames and heading'

--- twittler.js
+++ twittler.js
@@ -7,11 +7,12 @@ $(document).ready(function(){
   $body.append('<div class="tweettop"></div>')
   
   //This shows how many tweets are in the stream at the top.
-  $body.find('.tweettop').text(index)
+  $body.find('.tweettop').text('View ' +index+ ' New Tweet')
   
   while(index >= 0){
     var tweet = streams.home[index];
     
+    //Adding a html node to the body for each tweet with a class for the username, message and timestamp for styling.
     var $tweet = $('<div class="tweet"><span class = "username">@' + tweet.user +'</span><span class="message"> '+tweet.message+'</span><span class="timestamp"><br> '+tweet.created_at+'</span></div>');
    
     //Displays each tweet in the stream with the timestamp as well.
@@ -30,4 +31,4 @@ $(document).ready(function(){
 
 
 
-  });      
+});      
