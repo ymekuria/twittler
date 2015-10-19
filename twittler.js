d8f5fca15fad38c2df75bc99a6f53844929ae780
Yoni <ymekuria@gmail.com>
2015-10-19 12:29:11 -0700

    'Added the moment.js library and added the functionality to display a users tweets when a username is clicked'

--- twittler.js
+++ twittler.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
   $body.append('<div class="tweettop"></div>')
   
   //This shows how many tweets are in the stream at the top.
-  $body.find('.tweettop').text('View ' +index+ ' New Tweet')
+  $body.find('.tweettop').text('View ' +index+ ' New Tweets')
   
-  while(index >= 0){
+  while (index >= 0) {
     var tweet = streams.home[index];
     
     //Adding a html node to the body for each tweet with a class for the username, message and timestamp for styling.
-    var $tweet = $('<div class="tweet"><span class = "username">@' + tweet.user +'</span><span class="message"> '+tweet.message+'</span><span class="timestamp"><br> '+tweet.created_at+'</span></div>');
-   
+    var $tweet = $('<div class="tweet" ><span class = "username" data-user='+tweet.user+ '>@' + tweet.user +'</span><span class="message"> '+tweet.message+'</span><span class="timestamp"><br> '+moment(tweet.created_at)+'</span></div>');
+  
     //Displays each tweet in the stream with the timestamp as well.
    // $tweet.text('@' + tweet.user + ': ' + tweet.message+'\n');
     
@@ -23,11 +23,38 @@ $(document).ready(function(){
     index -= 1;
   }
   
-  $(document).on('click', '.tweet', function(){
-    //streams.
+
+  //This function displays a users tweets when a username is clicked
+  $(document).on('click', '.username', function() {
+
+    //This variable references the clicked username from the data attribute.
+    var clickeduser = $(this).data('user');
+    var $body = $('body');
+    var index = streams.users[clickeduser].length - 1;
+
+    $body.html('');
+    $body.append('<div class="tweettop"></div>')
+      
+    //This shows how many tweets are in the stream at the top.
+    $body.find('.tweettop').text('View ' +index+ ' New Tweets')
+   
+    
+    //This hides the previous tweet stream.
+    $('body').find('.tweet').hide(); 
+   
+    while (index >= 0) {
+      var tweet = streams.users[clickeduser][index];   
+      var $tweet = $('<div class="usertweet" ><span class = "username" data-user='+tweet.user+ '>@' + tweet.user +'</span><span class="message"> '+tweet.message+'</span><span class="timestamp"><br> '+moment(tweet.created_at)+'</span></div>');
+      $tweet.appendTo($body);
+      index -= 1;
+    } 
+
+  });
+   
+
 
     
-  })
+
 
 
 
